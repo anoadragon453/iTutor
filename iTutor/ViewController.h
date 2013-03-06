17360ab1f31a28416f73a80b1ef20e10b4495512
Andrew Morgan <andrew@andrews-macbook-pro.local>
2013-03-06 14:02:42 -0800

    Initial Commit

---
+++ iTutor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iTutor
+//
+//  Created by Andrew Morgan on 3/6/13.
+//  Copyright (c) 2013 Andrew Morgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iTutor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTutor
+//
+//  Created by Andrew Morgan on 3/6/13.
+//  Copyright (c) 2013 Andrew Morgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
